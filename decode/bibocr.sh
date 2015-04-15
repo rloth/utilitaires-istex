fbb8eadcedcd39470631f64354d22decbacf94f7
Romain Loth <romain.loth@inist.fr>
2015-04-15 16:02:49 +0200

    bibocr: oubli d'une dépendance

--- decode/bibocr.sh
+++ decode/bibocr.sh
@@ -9,7 +9,8 @@
 # ----------------------------------------------------
 # Dépendances:
 #  - pdfinfo   (deb package: poppler-utils)
-#  - convert   (deb package: imagemagick)
+#  - pdftk     (deb package: pdftk        )
+#  - convert   (deb package: imagemagick  )
 #  - tesseract (deb package: tesseract-ocr)
 # ----------------------------------------------------
 # (c) 2015 INIST-CNRS (ISTEX) romain.loth at inist.fr
@@ -21,20 +22,17 @@ export PDF_IN=$1
 export MAXPAGES=10
 export RESOLUTION=800  # doit être > 300x300 dpi
 
-# pour les logs sur STDERR
-alias errcho='>&2 echo'
-
 # info sur le nombre de pages total
 ACTUALPAGES=`pdfinfo $PDF_IN | grep -a Pages | grep -Po "\d+$"`
 
 # log du précédent
-errcho "PDFIN: $ACTUALPAGES pp"
+>&2 echo "PDFIN: $ACTUALPAGES pp"
 
 # le nombre de pages à partir de la fin qu'on va demander
 export N_pp=$(($ACTUALPAGES>$MAXPAGES?$MAXPAGES:$ACTUALPAGES))
 
 # log
-errcho "CONVERSION: $N_pp last pages"
+>&2 echo "CONVERSION: $N_pp last pages"
 
 # obtention de la fin du pdf dans un nouveau fichier pdf temporaire: 
 # ex: /run/shm/tmp.bibocr-frag-KPihH1.pdf
@@ -45,7 +43,7 @@ pdftk $PDF_IN cat r${N_pp}-r1 output $TMP_FRAG_PATH
 convert -density $RESOLUTION $TMP_FRAG_PATH ${TMP_FRAG_PATH}.converted-%02d.png
 
 # OCR-ization
-errcho "OCR-IZATION TO STDOUT..."
+>&2 echo "OCR-IZATION TO STDOUT..."
 for IMG in ${TMP_FRAG_PATH}.converted-*.png
  do tesseract $IMG stdout
     echo -ne "\f"
@@ -54,3 +52,5 @@ for IMG in ${TMP_FRAG_PATH}.converted-*.png
 # nettoyage des tmp dans la RAM (/run/shm)
 rm -fr $TMP_FRAG_PATH
 rm -fr ${TMP_FRAG_PATH}.converted-*.png
+
+>&2 echo "FINI"
